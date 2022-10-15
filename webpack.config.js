ba3a861415d51d8d3c8a190bb5b9a65b561ed26e
changw <2283992824@qq.com>
2022-10-15 17:37:27 +0800

    webpack成功生成js/html/css

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development', // development or production
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js',
+    filename: '[name].[contenthash].js',  //filename: 'index.[contenthash].js', 
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '小常',
+    template: 'src/assets/index.html'  //按照我给你的模板生成html文件
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,  //以.css结尾的用css-loader加载
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
